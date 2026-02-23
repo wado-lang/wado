b18fcfa2172325912e8f22d8ba435f5eb085e021
FUJI Goro <g.psy.va@gmail.com>
2026-02-23 23:31:28 +0900

    remove garbage

--- wasm-size/rust/hello_world/target/wasm32-wasip1/release/hello_world.d
+++
@@ -1,1 +0,0 @@
-/Users/goro/ghq/github.com/wado-lang/wado/wasm-size/rust/hello_world/target/wasm32-wasip1/release/hello_world.wasm: /Users/goro/ghq/github.com/wado-lang/wado/wasm-size/rust/hello_world/src/main.rs
--- wasm-size/rust/pi_approx/target/wasm32-wasip1/release/deps/pi_approx-e62e3547709bb250.d
+++
@@ -1,5 +0,0 @@
-/Users/goro/ghq/github.com/gfx/wado/wasm-size/rust/pi_approx/target/wasm32-wasip1/release/deps/pi_approx-e62e3547709bb250.d: src/main.rs
-
-/Users/goro/ghq/github.com/gfx/wado/wasm-size/rust/pi_approx/target/wasm32-wasip1/release/deps/pi_approx-e62e3547709bb250.wasm: src/main.rs
-
-src/main.rs:
