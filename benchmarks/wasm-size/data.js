98b838f386d7871eea9c001a81cca489c159b915
github-action-benchmark <github@users.noreply.github.com>
2026-04-16 13:23:02 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 8b49a567248e19ab059efdf60ddf80594e8067a1

--- benchmarks/wasm-size/data.js
+++ benchmarks/wasm-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776345608411,
+  "lastUpdate": 1776345782882,
   "repoUrl": "https://github.com/wado-lang/wado",
   "entries": {
     "Benchmark": [
@@ -13244,6 +13244,45 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/wado-lang/wado/commit/c8e43ed380ed098abfca801a1ebcd9720edbe2b0"
         },
         "date": 1776345608118,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "hello_world",
+            "value": 1773,
+            "unit": "bytes"
+          },
+          {
+            "name": "pi_approx",
+            "value": 8943,
+            "unit": "bytes"
+          },
+          {
+            "name": "zlib",
+            "value": 18727,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "g.psy.va@gmail.com",
+            "name": "FUJI Goro",
+            "username": "gfx"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8b49a567248e19ab059efdf60ddf80594e8067a1",
+          "message": "Merge pull request #850 from wado-lang/gfx/update-deps-vscode-ext\n\nchore: update package-lock.json",
+          "timestamp": "2026-04-16T22:19:27+09:00",
+          "tree_id": "0b15563d5f3224f100c169fbe347ce06ea7bfef3",
+          "url": "https://github.com/wado-lang/wado/commit/8b49a567248e19ab059efdf60ddf80594e8067a1"
+        },
+        "date": 1776345782605,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
