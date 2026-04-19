ffb1fae50982bcd8276cab4fe0fa13966eb564e5
github-action-benchmark <github@users.noreply.github.com>
2026-04-19 16:32:26 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 78110f2970d79d705a037a61b28b0f00a5bacec1

--- benchmarks/wasm-size/data.js
+++ benchmarks/wasm-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776603477278,
+  "lastUpdate": 1776616346000,
   "repoUrl": "https://github.com/wado-lang/wado",
   "entries": {
     "Benchmark": [
@@ -14944,6 +14944,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "zlib",
             "value": 18670,
+            "unit": "bytes"
+          },
+          {
+            "name": "sqlite_highlight",
+            "value": 618057,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "g.psy.va@gmail.com",
+            "name": "FUJI Goro",
+            "username": "gfx"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "78110f2970d79d705a037a61b28b0f00a5bacec1",
+          "message": "Merge pull request #892 from wado-lang/gfx/employ_default_values\n\nfix: improve stdlib APIs with default values, fixing method resolution with generics",
+          "timestamp": "2026-04-20T01:28:19+09:00",
+          "tree_id": "0cd9503b13746ef3164ebcb39dbe60453cbbd27a",
+          "url": "https://github.com/wado-lang/wado/commit/78110f2970d79d705a037a61b28b0f00a5bacec1"
+        },
+        "date": 1776616345694,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "hello_world",
+            "value": 1773,
+            "unit": "bytes"
+          },
+          {
+            "name": "pi_approx",
+            "value": 8943,
+            "unit": "bytes"
+          },
+          {
+            "name": "zlib",
+            "value": 18896,
             "unit": "bytes"
           },
           {
